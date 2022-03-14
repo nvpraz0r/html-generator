cbac2632e18fd8aed2b5823fdc379afbe5d6cf4c
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-03-14 12:52:28 -0500

    Initial file set up
    Writing notes for the intended function for Generator.java

---
+++ src/Generator.java
@@ -0,0 +1,18 @@
+/**
+ * @author Jon
+ * @version 0.1
+ * 
+ * Notes
+ *  -This program will prompt user input via CLI
+ *  -Find indicated file
+ *  -Then convert the file to HTML
+ *  -Followed by generating the contents of the file into the format of an HTML file
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Generator {
+    public static void main(String[] args) {
+    }
+}
